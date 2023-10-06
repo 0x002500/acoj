95646d88775c9a70f7453d792ae3ee319c7629e4
0x002500 <tiger_pan@outlook.com>
2023-10-06 09:35:00 +0800

    [feat] mount the "/submit" route and add the compilers.rs to make the file tree more clean

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,9 @@ use rocket::serde::json::{serde_json::json, Json, Value};
 
 #[macro_use] extern crate rocket;
 
+//引用用于编译compiler     import the compiler for compile
+mod compiler;
+
 //定义提交结构体  define submit struct
 struct Submit {
     lang: String,
@@ -19,11 +22,12 @@ fn index() -> &'static str {
 }
 
 #[post("/submit", format = "json", data = "<submit>")]
-fn submit -> &'static str{
+fn submit() -> &'static str{
     "Already post code"
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+    rocket::build().mount("/submit", routes![submit])
 }
