f94bf12cda9a968e9001ae1df474873d9c14b8cd
0x002500 <tiger_pan@outlook.com>
2023-10-06 08:59:17 +0800

    [feat] add the "/submit" interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::serde::json::{serde_json::json, Json, Value};
+
+#[macro_use] extern crate rocket;
+
+//定义提交结构体  define submit struct
+struct Submit {
+    lang: String,
+    code: String
+}
+
+struct Re {
+    status: String,
+    wasm: String
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/submit", format = "json", data = "<submit>")]
+fn submit -> &'static str{
+    "Already post code"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
