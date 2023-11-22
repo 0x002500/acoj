1465af7d7e4ad8adee4a0c6a65e6e949409738db
0x002500 <tiger_pan@outlook.com>
2023-11-22 19:07:14 +0800

    [feat] add a based request function to seele.rs

--- backend/src/judger/seele.rs
+++ backend/src/judger/seele.rs
@@ -1,1 +1,7 @@
 use reqwest;
+
+async fn request_seele (url: &str) -> Result<String, reqwest::Error>{
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
+}
