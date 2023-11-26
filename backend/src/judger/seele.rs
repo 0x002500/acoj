1e55a56473b4a61c8b545d73e8956ad23fbf2b06
0x002500 <tiger_pan@outlook.com>
2023-11-26 21:09:41 +0800

    [des] add a dependencies for yaml

--- backend/src/judger/seele.rs
+++ backend/src/judger/seele.rs
@@ -1,4 +1,11 @@
 use reqwest;
+use serde_yaml;
+use serde::{Serialize, Deserialize};
+
+async fn build_res (res: &str) -> serde_yaml::Result<serde_yaml::Value> {
+    let res = serde_yaml::from_str(res);
+    res
+}
 
 async fn request_seele (url: &str) -> Result<String, reqwest::Error>{
     let response = reqwest::get(url).await?;
