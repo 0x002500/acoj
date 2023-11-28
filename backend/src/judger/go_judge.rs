fafef3563be469105cd65559b2e3c99a88439aa7
0x002500 <tiger_pan@outlook.com>
2023-11-28 19:19:53 +0800

    [feat] add some basic code in go_judge.rs

---
+++ backend/src/judger/go_judge.rs
@@ -0,0 +1,8 @@
+use reqwest;
+use serde::{Deserialize,Serialize};
+
+async fn request (url: &str) -> Result<String, reqwest::Error>{
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    Ok(body)
+}
