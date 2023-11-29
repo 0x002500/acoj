b1643ccaf16e7e86e9a2d83d42386a4be3204205
0x002500 <tiger_pan@outlook.com>
2023-11-29 23:18:32 +0800

    [water] water water water

--- backend/src/judger/go_judge.rs
+++ backend/src/judger/go_judge.rs
@@ -1,6 +1,8 @@
 use reqwest;
 use serde::{Deserialize,Serialize};
 
+pub struct GoJudge;
+
 async fn request (url: &str) -> Result<String, reqwest::Error>{
     let response = reqwest::get(url).await?;
     let body = response.text().await?;
